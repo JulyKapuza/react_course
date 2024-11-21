7e9709196cae18ef9439164da12246470b08ca4d
Julia <julia@Noutbuk-Julia.local>
2024-11-21 15:48:17 +0200

    finished ref and portal

--- 08/src/App.jsx
+++ 08/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
       <div id="challenges">
         <TimerChallenge title="Easy" targetTime={1} />
         <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting Tough" targetTime={10} />
+        <TimerChallenge title="Hard" targetTime={15} />
       </div>
     </>
   );
